6c678d87d91e99f8a7a22a6084dc78edf412b0b8
multapaucis <matthewrfree@gmail.com>
2018-08-03 17:56:13 -0700

    Switch Ajax Requests from Sync to Async

--- js/neighborhood.js
+++ js/neighborhood.js
@@ -1,14 +1,14 @@
-//Google Map Functionality
+//Google Map & Markers Set Up
 var map;
 var markers = [];
-var largeInfowindow = null;
+var infowindow = null;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 39.920541, lng: -105.086650},
         zoom: 11
       });
-    largeInfowindow = new google.maps.InfoWindow();
+    infowindow = new google.maps.InfoWindow();
     createMarkers();
 };
 
@@ -23,14 +23,16 @@ function createMarkers() {
             id:i
           });
         marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
+            populateInfoWindow(this);
         })
+        //adds marker to the list
         markers.push(marker);
+        //Assigns the Marker to the current Map
         marker.setMap(map);
     }
 };
 
-function populateInfoWindow(marker, infowindow) {
+function populateInfoWindow(marker) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
         // Clear the infowindow content to give the streetview time to load.
@@ -40,57 +42,64 @@ function populateInfoWindow(marker, infowindow) {
         infowindow.addListener('closeclick', function() {
             infowindow.marker = null;
         });
-        var weatherData = getWeather(marker.position);
-        var weather = weatherData[0];
-        var temp = weatherData[1];
-        var safety = safeTemp(temp);
-        //console.log(weather);
-        infowindow.setContent('<div class="info">' + marker.title +'<br>'+ weather+
-        " " +temp+ ' degrees<br>' +safety+ '</div>');
+        prepInfowindow(marker);
         // Open the infowindow on the correct marker.
         infowindow.open(map, marker);
     };
   };
 
-function getWeather(position) {
+function prepInfowindow(marker) {
     //Retrieve Location information from Marker
-    var lat = position.lat();
-    var lng = position.lng();
+    var lat = marker.position.lat();
+    var lng = marker.position.lng();
+    var infoTemplate = '<div class="info"><strong>' + marker.title +'</strong>';
+
     //Retrieve Location Key
     var locKey = null;
      $.ajax({
         type: 'GET',
         url: "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?",
         dataType: 'json',
-        success: function(results) {
-            locKey = results['Key'];
-        },
         data: {
             "apikey": "4jAd6Gz60fmd8Q1vCn238AswWOjdZtzt",
             "q": lat + "," + lng
         },
-        async: false
+        async: true
+    }).done( function(results){
+        locKey = results['Key'];
+        getWeather(locKey, infoTemplate);
+    }).fail( function(){
+        infoTemplate += '<br>' + 'Unable to Retrieve Weather Data</div>';
+        infowindow.setContent(infoTemplate);
+        return infoTemplate;
     });
+};
+
+function getWeather(locKey, infoTemplate) {
     //Retrieve Location Weather information
     var weatherText = null;
     var weatherTemp = null;
-    var weatherURL = "http://dataservice.accuweather.com/currentconditions/v1/" + locKey
-     $.ajax({
+    var weatherURL = "http://dataservice.accuweather.com/currentconditions/v1/" + locKey;
+    $.ajax({
         type: 'GET',
         url: weatherURL,
         dataType: 'json',
-        success: function(results) {
-            console.log(results[0]);
+        data: {
+            "apikey": "4jAd6Gz60fmd8Q1vCn238AswWOjdZtzt"
+        },
+        async: true
+    }).done( function(results){
             weatherText = results[0]['WeatherText'];
             weatherTemp = results[0]['Temperature']['Imperial']['Value'];
-        },
-        data: {
-            "apikey": "4jAd6Gz60fmd8Q1vCn238AswWOjdZtzt",
-            "q": lat + "," + lng
-        },
-        async: false
+            var safety = safeTemp(weatherTemp);
+            infoTemplate += '<br>'+ weatherText+ " at " +weatherTemp+ ' degrees<br>' +safety+ '</div>';
+            infowindow.setContent(infoTemplate);
+            return infoTemplate;
+    }).fail( function(){
+        infoTemplate += '<br>' + 'Unable to Retrieve Weather Data</div>';
+        infowindow.setContent(infoTemplate);
+        return infoTemplate;
     });
-    return [weatherText, weatherTemp];
 
 };
 
@@ -108,9 +117,7 @@ function safeTemp(currentTemp) {
         safetyRating = "Too Hot";
     };
     return safetyRating;
-
 };
-
 
 
 //Knockout Functionality
@@ -138,77 +145,47 @@ var Location = function(data){
     this.locLatLng = ko.observable(data.latlng);
     this.city = ko.observable(data.city);
     this.showing = ko.observable(true);
+    this.marker = null;
 }
 
 
-/*var infoWindowData = function(data) {
-    this.title = ko.observable(data.title);
-    this.weather = ko.observable(data.weather);
-    this.temp = ko.observable(data.temp + ' degrees');
-    this.recommend = ko.computed(function(){
-        var currentTemp = this.temp();
-        var safetyRating = null;
-        if (currentTemp < 32) {
-            safetyRating = "Too Cold";
-        } else if (currentTemp < 40) {
-            safetyRating = "Careful It's Getting Cold";
-        } else if (currentTemp < 75) {
-            safetyRating = "Perfect Temp for Play";
-        } else if (currentTemp < 85) {
-            safetyRating = "Careful It's Getting Hot";
-        } else {
-            safetyRating = "Too Hot";
-        };
-        return safetyRating;
-    }, this);
-}
-*/
 var ViewModel = function(){
     var self = this;
 
+    //Create Observable Array of Locations
     self.locationList = ko.observableArray([]);
-    self.availableCities = ko.observableArray(['Show All Parks']);
+    //Start page with list of parks showing
     self.listShowing = ko.observable(true);
+    //Create Observable array of Cities for the Filter
+    self.availableCities = ko.observableArray(['Show All Parks']);
     self.chosenCity = ko.observable('Show All Parks');
-
+    //Add all locations to the Observable Array as KO Objects
     locations.forEach(function(locItem){
-        //Add location to list as KO Object
-        self.locationList.push( new Location(locItem));
-        //Add cities of locations to available cities list
-        if (self.availableCities.indexOf(locItem.city) == -1) {
-            self.availableCities.push(locItem.city);
+        var newLoc = new Location(locItem)
+        markers.forEach(function(mark){
+            console.log(mark);
+            if (mark.title == newLoc.locName()){
+                console.log(mark);
+                newLoc.marker(mark);
+            };
+        })
+        self.locationList.push(newLoc);
+
+        //Add cities of locations to available cities list if not already included
+        if (self.availableCities.indexOf(newLoc.city()) == -1) {
+            self.availableCities.push(newLoc.city());
         };
     });
 
-
-    /*self.currentInfo = null;
-    self.createMarkers = function() {
-        var largeInfowindow = new google.maps.InfoWindow();
-        for (var i = 0; i < self.locationList.length; i++) {
-        //Create a marker for the location
-            if (locationList[i].showing) {
-                var marker = new google.maps.Marker({
-                    position: locationList[i].latlng,
-                    title: locationList[i].name,
-                    animation: google.maps.Animation.DROP,
-                    id:i
-                  });
-                marker.addListener('click', function() {
-                    ViewModel.setInfoWindow(this, largeInfowindow);
-                })
-                markers.push(markers);
-                marker.setMap(map);
-            };
-        };
-    };*/
-    //self.createMarkers();
     self.selectLocation = function(location) {
         //Find Marker that matches location
         markers.forEach(function(mark){
             if (mark.title == location.locName()) {
-                populateInfoWindow(mark, largeInfowindow);
+                //If it matches show Info Window
+                populateInfoWindow(mark);
             };
         })
+        updateMarkers();
     };
     self.hideList = function() {
         self.listShowing(false);
@@ -216,92 +193,23 @@ var ViewModel = function(){
     self.showList = function() {
         self.listShowing(true);
     };
-    self.showPark = function(city) {
+    self.showPark = function(city, showing) {
+        var toShow = null;
         if (chosenCity() == 'Show All Parks') {
-            return true;
+            toShow = true;
+            showing(true);
         } else if (city() == chosenCity()){
-            return true;
+            toShow = true;
+            showing(true);
         } else {
-            return false;
+            toShow = false;
+            showing(false);
         }
+        return toShow;
     };
-    /*self.getWeather = function(position) {
-        //Retrieve Location information from Marker
-        var lat = position.lat();
-        var lng = position.lng();
-        //Retrieve Location Key
-        var locKey = null;
-         $.ajax({
-            type: 'GET',
-            url: "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?",
-            dataType: 'json',
-            success: function(results) {
-                locKey = results['Key'];
-            },
-            data: {
-                "apikey": "4jAd6Gz60fmd8Q1vCn238AswWOjdZtzt",
-                "q": lat + "," + lng
-            },
-            async: false
-        });
-        //Retrieve Location Weather information
-        var weatherText = null;
-        var weatherTemp = null;
-        var weatherURL = "http://dataservice.accuweather.com/currentconditions/v1/" + locKey
-         $.ajax({
-            type: 'GET',
-            url: weatherURL,
-            dataType: 'json',
-            success: function(results) {
-                console.log(results[0]);
-                weatherText = results[0]['WeatherText'];
-                weatherTemp = results[0]['Temperature']['Imperial']['Value'];
-            },
-            data: {
-                "apikey": "4jAd6Gz60fmd8Q1vCn238AswWOjdZtzt",
-                "q": lat + "," + lng
-            },
-            async: false
-        });
-        return [weatherText, weatherTemp];
-    }
-    self.setCurrentInfo =function(marker) {
-            var weatherData = self.getWeather(marker.position);
-            var weatherSum = weatherData[0];
-            var curTemp = weatherData[1];
-            var safety = safeTemp(temp);
-            self.currentInfo( new infoWindowData(
-                {
-                    title: marker.title,
-                    weather: weatherSum,
-                    temp: curTemp
-            }));
-
+    self.updateMarkers = function() {
+        console.log(locationList());
     };
-
-
-    self.setInfoWindow = function(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-            // Clear the infowindow content to give the streetview time to load.
-            infowindow.setContent('');
-            infowindow.marker = marker;
-            // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick', function() {
-                infowindow.marker = null;
-            });
-            self.setCurrentInfo(marker);
-            //contensole.log(weather);
-            infowindow.setContent('<div data-bind:"with: self.currentInfo()">' +
-                '<div data-bind="text: title></div>' +
-                '<div data-bind="text: weather></div>' +
-                '<div data-bind="text: temp</div>' +
-                '<div data-bind="text: recommend></div>' +
-                '</div>');
-            // Open the infowindow on the correct marker.
-            infowindow.open(map, marker);
-        };
-    }*/
 
 }
 
